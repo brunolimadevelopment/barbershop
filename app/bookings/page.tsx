adef7d1bb8b25c53d650fb3e3a3ffd05bbf0b368
Bruno Lima <brunolimadevelopment@gmail.com>
2024-02-09 16:49:29 -0300

    feat: ensure confirmed bookings are shown on home page

--- app/bookings/page.tsx
+++ app/bookings/page.tsx
@@ -15,19 +15,37 @@ const BookingsPage = async () => {
         return redirect("/");
     }
 
-    // pega os agendamentos do usuário
-    const bookings = await db.booking.findMany({
-        where: {
-            userId: (session.user as any).id,
-        },
-        include: {
-            service: true,
-            barbershop: true,  
-        }
-    })
-
-    const confirmedBookings = bookings.filter(booking => isFuture(booking.date))
-    const finishedBookings = bookings.filter(booking => isPast(booking.date))
+    // pega os agendamentos confirmados e finalizados do usuário
+    // 2 Promise paralelamente - fica mais rápido
+    const [confirmedBookings, fineshidBookings] = await Promise.all([
+        db.booking.findMany({
+            where: {
+                userId: (session.user as any).id,
+                date: {
+                    gte: new Date(),
+                }
+            },
+            include: {
+                service: true,
+                barbershop: true,  
+            }
+        }),
+        db.booking.findMany({
+            where: {
+                userId: (session.user as any).id,
+                date: {
+                    lt: new Date(),
+                }
+            },
+            include: {
+                service: true,
+                barbershop: true,  
+            }
+        })
+    ])
+    // fazer o filter com o date-ns via js consome mais memoria.
+    //const confirmedBookings = bookings.filter(booking => isFuture(booking.date))
+    //const finishedBookings = bookings.filter(booking => isPast(booking.date))
     
 
     return ( 
@@ -44,7 +62,7 @@ const BookingsPage = async () => {
 
                 <h2 className="text-gray-400 uppercase font-bold text-sm mt-6 mb-3">Finalizados</h2>
                 <div className="flex flex-col gap-3">
-                    {finishedBookings.map(booking =>(
+                    {fineshidBookings.map(booking =>(
                         <BookingItem key={booking.id} booking={booking} />
                     ))}
                 </div>
